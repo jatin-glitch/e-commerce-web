54520f2162ff083617f5dbcc4ef57ef2fbc822c4
jatin-glitch <jvalecha50@gmail.com>
2026-02-14 16:45:28 +0500

    Fix image path to backend/Images folder

--- backend/src/routes/imageRoutes.js
+++ backend/src/routes/imageRoutes.js
@@ -8,6 +8,6 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 // Serve static images from the Images folder
-router.use('/Images', express.static(path.join(__dirname, '../Images')));
+router.use('/Images', express.static(path.join(__dirname, '../../Images')));
 
 export default router;
